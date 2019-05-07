571c8b2470c2c8e30a17822804a482adc52b27c3
ccckmit <ccckmit@gmail.com>
2019-05-07 09:56:34 +0800

    output asm in MSYS2 -fverbose-asm

--- code/c/03-asm/01-add/add.s
+++ code/c/03-asm/01-add/add.s
@@ -1,26 +1,68 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 13
+	.file	"add.c"
+ # GNU C11 (Rev2, Built by MSYS2 project) version 7.3.0 (i686-w64-mingw32)
+ #	compiled by GNU C version 7.3.0, GMP version 6.1.2, MPFR version 4.0.1, MPC version 1.1.0, isl version isl-0.18-GMP
+
+ # GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+ # options passed: 
+ # -iprefix C:/msys64/mingw32/bin/../lib/gcc/i686-w64-mingw32/7.3.0/
+ # -D_REENTRANT add.c -mtune=generic -march=i686 -auxbase-strip add.s
+ # -fverbose-asm
+ # options enabled:  -faggressive-loop-optimizations
+ # -fasynchronous-unwind-tables -fauto-inc-dec -fchkp-check-incomplete-type
+ # -fchkp-check-read -fchkp-check-write -fchkp-instrument-calls
+ # -fchkp-narrow-bounds -fchkp-optimize -fchkp-store-bounds
+ # -fchkp-use-static-bounds -fchkp-use-static-const-bounds
+ # -fchkp-use-wrappers -fcommon -fdelete-null-pointer-checks
+ # -fdwarf2-cfi-asm -fearly-inlining -feliminate-unused-debug-types
+ # -ffp-int-builtin-inexact -ffunction-cse -fgcse-lm -fgnu-runtime
+ # -fgnu-unique -fident -finline-atomics -fira-hoist-pressure
+ # -fira-share-save-slots -fira-share-spill-slots -fivopts
+ # -fkeep-inline-dllexport -fkeep-static-consts -fleading-underscore
+ # -flifetime-dse -flto-odr-type-merging -fmath-errno -fmerge-debug-strings
+ # -fpeephole -fplt -fprefetch-loop-arrays -freg-struct-return
+ # -fsched-critical-path-heuristic -fsched-dep-count-heuristic
+ # -fsched-group-heuristic -fsched-interblock -fsched-last-insn-heuristic
+ # -fsched-rank-heuristic -fsched-spec -fsched-spec-insn-heuristic
+ # -fsched-stalled-insns-dep -fschedule-fusion -fsemantic-interposition
+ # -fset-stack-executable -fshow-column -fshrink-wrap-separate
+ # -fsigned-zeros -fsplit-ivs-in-unroller -fssa-backprop -fstdarg-opt
+ # -fstrict-volatile-bitfields -fsync-libcalls -ftrapping-math
+ # -ftree-cselim -ftree-forwprop -ftree-loop-if-convert -ftree-loop-im
+ # -ftree-loop-ivcanon -ftree-loop-optimize -ftree-parallelize-loops=
+ # -ftree-phiprop -ftree-reassoc -ftree-scev-cprop -funit-at-a-time
+ # -funwind-tables -fverbose-asm -fzero-initialized-in-bss -m32 -m80387
+ # -m96bit-long-double -maccumulate-outgoing-args -malign-double
+ # -malign-stringops -mavx256-split-unaligned-load
+ # -mavx256-split-unaligned-store -mfancy-math-387 -mfp-ret-in-387
+ # -mieee-fp -mlong-double-80 -mms-bitfields -mno-red-zone -mno-sse4
+ # -mpush-args -msahf -mstack-arg-probe -mstv -mvzeroupper
+
+	.text
 	.globl	_add
-	.p2align	4, 0x90
-_add:                                   ## @add
+	.def	_add;	.scl	2;	.type	32;	.endef
+_add:
+LFB0:
 	.cfi_startproc
-## BB#0:
-	pushq	%rbp
-Lcfi0:
-	.cfi_def_cfa_offset 16
-Lcfi1:
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-Lcfi2:
-	.cfi_def_cfa_register %rbp
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	movl	-4(%rbp), %esi
-	addl	-8(%rbp), %esi
-	movl	%esi, %eax
-	popq	%rbp
-	retq
+	pushl	%ebp	 #
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp	 #,
+	.cfi_def_cfa_register 5
+	subl	$16, %esp	 #,
+ # add.c:2:   int t = a, x=b;
+	movl	8(%ebp), %eax	 # a, tmp89
+	movl	%eax, -4(%ebp)	 # tmp89, t
+	movl	12(%ebp), %eax	 # b, tmp90
+	movl	%eax, -8(%ebp)	 # tmp90, x
+ # add.c:3:   return a+b;
+	movl	8(%ebp), %edx	 # a, tmp91
+	movl	12(%ebp), %eax	 # b, tmp92
+	addl	%edx, %eax	 # tmp91, _5
+ # add.c:4: }
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
 	.cfi_endproc
-
-
-.subsections_via_symbols
+LFE0:
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 7.3.0"
